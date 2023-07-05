7ba0d7b08ac33ea33362b10b5d2fa51e9151eead
crist <crist@192.168.1.7>
2023-07-05 00:22:55 -0500

    Tiene una mejor imagen y es mas agradable a las vista

--- ExamenFinalLP3/src/codigo/GUILexico.java
+++ ExamenFinalLP3/src/codigo/GUILexico.java
@@ -17,6 +17,8 @@ import javax.swing.JTextArea;
 import javax.swing.JTextPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
 
 public class GUILexico {
 
@@ -51,11 +53,6 @@ public class GUILexico {
         frmProgramaDeAnalizador.setBounds(100, 100, 747, 759);
         frmProgramaDeAnalizador.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JTextPane txtpnCodigoAEvaluar = new JTextPane();
-        txtpnCodigoAEvaluar.setBackground(new Color(192, 192, 192));
-        txtpnCodigoAEvaluar.setFont(txtpnCodigoAEvaluar.getFont().deriveFont(txtpnCodigoAEvaluar.getFont().getStyle() | Font.BOLD, txtpnCodigoAEvaluar.getFont().getSize() + 13f));
-        txtpnCodigoAEvaluar.setText("Codigo a Evaluar");
-
         JButton btnNewButton = new JButton("Analizar");
         btnNewButton.setBackground(new Color(140, 253, 154));
         btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
@@ -86,27 +83,6 @@ public class GUILexico {
             }
         });
 
-        JTextPane txtpnLexicoUtilizado = new JTextPane();
-        txtpnLexicoUtilizado.setBackground(new Color(192, 192, 192));
-        txtpnLexicoUtilizado.setText("Lexico Utilizado");
-        txtpnLexicoUtilizado.setFont(txtpnLexicoUtilizado.getFont().deriveFont(txtpnLexicoUtilizado.getFont().getStyle() | Font.BOLD, txtpnLexicoUtilizado.getFont().getSize() + 13f));
-
-        JTextPane txtpnPor = new JTextPane();
-        txtpnPor.setBackground(new Color(230, 230, 250));
-        txtpnPor.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        txtpnPor.setText("Por: ccarrizalesl@ulasalle.edu.pe");
-
-        JTextPane txtpnGitHubHttpsgithubcomkoldozmma = new JTextPane();
-        txtpnGitHubHttpsgithubcomkoldozmma.setBackground(new Color(230, 230, 250));
-        txtpnGitHubHttpsgithubcomkoldozmma.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        txtpnGitHubHttpsgithubcomkoldozmma.setText("Git hub: https://github.com/KoldozMMA3");
-
-        JTextPane txtpnAnalizadorLexicoC = new JTextPane();
-        txtpnAnalizadorLexicoC.setForeground(Color.RED);
-        txtpnAnalizadorLexicoC.setBackground(SystemColor.info);
-        txtpnAnalizadorLexicoC.setText("Analizador Lexico C++");
-        txtpnAnalizadorLexicoC.setFont(new Font("Impact", Font.PLAIN, 49));
-
         textArea_1 = new JTextArea();
         textArea_1.setBackground(new Color(152, 251, 152));
 
@@ -121,49 +97,72 @@ public class GUILexico {
         btnLimpiar.setFont(new Font("Tahoma", Font.PLAIN, 17));
 
         JScrollPane scrollPane = new JScrollPane();
+        
+        JLabel lblNewJgoodiesLabel = DefaultComponentFactory.getInstance().createLabel("Analizador Lexico C++");
+        lblNewJgoodiesLabel.setFont(new Font("Tahoma", Font.BOLD, 39));
+        lblNewJgoodiesLabel.setForeground(Color.RED);
+        
+        JLabel lblNewJgoodiesLabel_1 = DefaultComponentFactory.getInstance().createLabel("Por: ccarrizalesl@ulasalle.edu.pe");
+        lblNewJgoodiesLabel_1.setBackground(SystemColor.activeCaptionBorder);
+        lblNewJgoodiesLabel_1.setFont(new Font("Tahoma", Font.BOLD, 17));
+        
+        JLabel lblNewJgoodiesLabel_2 = DefaultComponentFactory.getInstance().createLabel("https://github.com/KoldozMMA3");
+        lblNewJgoodiesLabel_2.setFont(new Font("Tahoma", Font.BOLD, 17));
+        
+        JLabel lblNewJgoodiesTitle = DefaultComponentFactory.getInstance().createTitle("Codigo a Evaluar");
+        lblNewJgoodiesTitle.setForeground(SystemColor.textHighlight);
+        lblNewJgoodiesTitle.setBackground(SystemColor.activeCaption);
+        lblNewJgoodiesTitle.setFont(new Font("Tahoma", Font.BOLD, 24));
+        
+        JLabel lblNewJgoodiesTitle_1 = DefaultComponentFactory.getInstance().createTitle("Lexico Utilizado");
+        lblNewJgoodiesTitle_1.setForeground(SystemColor.textHighlight);
+        lblNewJgoodiesTitle_1.setBackground(SystemColor.textHighlight);
+        lblNewJgoodiesTitle_1.setFont(new Font("Tahoma", Font.BOLD, 24));
 
         GroupLayout groupLayout = new GroupLayout(frmProgramaDeAnalizador.getContentPane());
         groupLayout.setHorizontalGroup(
         	groupLayout.createParallelGroup(Alignment.LEADING)
         		.addGroup(groupLayout.createSequentialGroup()
-        			.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+        			.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
         				.addGroup(groupLayout.createSequentialGroup()
         					.addGap(20)
         					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-        						.addComponent(txtpnGitHubHttpsgithubcomkoldozmma, GroupLayout.PREFERRED_SIZE, 266, GroupLayout.PREFERRED_SIZE)
         						.addGroup(groupLayout.createSequentialGroup()
         							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
         								.addGroup(groupLayout.createSequentialGroup()
-        									.addGap(38)
-        									.addComponent(txtpnCodigoAEvaluar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-        								.addComponent(txtpnPor, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-        							.addGap(63)
-        							.addGap(43)
-        							.addComponent(txtpnLexicoUtilizado, GroupLayout.PREFERRED_SIZE, 210, GroupLayout.PREFERRED_SIZE))
+        									.addComponent(textArea_1, GroupLayout.PREFERRED_SIZE, 324, GroupLayout.PREFERRED_SIZE)
+        									.addGap(18)
+        									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+        										.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 343, GroupLayout.PREFERRED_SIZE)
+        										.addGroup(groupLayout.createSequentialGroup()
+        											.addGap(72)
+        											.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 128, GroupLayout.PREFERRED_SIZE)
+        											.addPreferredGap(ComponentPlacement.RELATED)
+        											.addComponent(btnLimpiar, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE))))
+        								.addComponent(lblNewJgoodiesLabel_2, GroupLayout.PREFERRED_SIZE, 313, GroupLayout.PREFERRED_SIZE)
+        								.addComponent(lblNewJgoodiesLabel_1))
+        							.addPreferredGap(ComponentPlacement.RELATED, 0, Short.MAX_VALUE))
         						.addGroup(groupLayout.createSequentialGroup()
-        							.addComponent(textArea_1, GroupLayout.PREFERRED_SIZE, 324, GroupLayout.PREFERRED_SIZE)
-        							.addGap(18)
-        							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-        								.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 343, GroupLayout.PREFERRED_SIZE)
-        								.addGroup(groupLayout.createSequentialGroup()
-        									.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 128, GroupLayout.PREFERRED_SIZE)
-        									.addPreferredGap(ComponentPlacement.RELATED)
-        									.addComponent(btnLimpiar, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE))))))
+        							.addGap(60)
+        							.addComponent(lblNewJgoodiesTitle)
+        							.addPreferredGap(ComponentPlacement.RELATED, 153, Short.MAX_VALUE)
+        							.addComponent(lblNewJgoodiesTitle_1)
+        							.addGap(81))))
         				.addGroup(groupLayout.createSequentialGroup()
-        					.addGap(140)
-        					.addComponent(txtpnAnalizadorLexicoC, GroupLayout.PREFERRED_SIZE, 482, GroupLayout.PREFERRED_SIZE)))
+        					.addGap(125)
+        					.addComponent(lblNewJgoodiesLabel, GroupLayout.PREFERRED_SIZE, 468, GroupLayout.PREFERRED_SIZE)))
         			.addContainerGap(26, Short.MAX_VALUE))
         );
         groupLayout.setVerticalGroup(
         	groupLayout.createParallelGroup(Alignment.LEADING)
         		.addGroup(groupLayout.createSequentialGroup()
-        			.addGap(22)
-        			.addComponent(txtpnAnalizadorLexicoC, GroupLayout.PREFERRED_SIZE, 68, GroupLayout.PREFERRED_SIZE)
-        			.addGap(37)
-        			.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-        				.addComponent(txtpnLexicoUtilizado, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-        				.addComponent(txtpnCodigoAEvaluar, GroupLayout.DEFAULT_SIZE, 35, Short.MAX_VALUE))
-        			.addPreferredGap(ComponentPlacement.RELATED, 73, Short.MAX_VALUE)
+        			.addGap(20)
+        			.addComponent(lblNewJgoodiesLabel, GroupLayout.PREFERRED_SIZE, 57, GroupLayout.PREFERRED_SIZE)
+        			.addGap(24)
+        			.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(lblNewJgoodiesTitle)
+        				.addComponent(lblNewJgoodiesTitle_1))
+        			.addPreferredGap(ComponentPlacement.RELATED, 14, Short.MAX_VALUE)
         			.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
         				.addGroup(groupLayout.createSequentialGroup()
         					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 397, GroupLayout.PREFERRED_SIZE)
@@ -173,11 +172,11 @@ public class GUILexico {
         						.addComponent(btnLimpiar, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
         					.addGap(7))
         				.addComponent(textArea_1, GroupLayout.PREFERRED_SIZE, 459, GroupLayout.PREFERRED_SIZE))
-        			.addGap(18)
-        			.addComponent(txtpnPor, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-        			.addPreferredGap(ComponentPlacement.RELATED)
-        			.addComponent(txtpnGitHubHttpsgithubcomkoldozmma, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-        			.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        			.addGap(38)
+        			.addComponent(lblNewJgoodiesLabel_1)
+        			.addPreferredGap(ComponentPlacement.UNRELATED)
+        			.addComponent(lblNewJgoodiesLabel_2)
+        			.addGap(26))
         );
         consoleTextArea = new JTextArea();
         consoleTextArea.setBackground(new Color(144, 238, 144));
